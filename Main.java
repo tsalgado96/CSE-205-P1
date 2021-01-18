608664743588b5a0e3a49b085c851b23fa52c645
TJ Salgado <tsalgado96@gmail.com>
2021-01-18 09:43:59 +0100

    Added try catch satement for FileNotFoundException

--- Main.java
+++ Main.java
@@ -13,6 +13,10 @@
  * AUTHOR 2: Isaac Pena, ipena5, ippenaisaac@gmail.com
  * AUTHOR 3: Stephen Elledge, saelledg, saelledg@asu.edu
  ********************************************************************************************************/
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] pArgs) {
@@ -20,7 +24,21 @@ public class Main {
     }
 
     private void run(){
-        System.out.println("Hello world");
-		System.out.println("Hello again world");
+        // User inputs the name of the file
+//        Scanner input = new Scanner((System.in));
+//        System.out.print("Please enter file name: ");
+//        String fileName = input.next();
+
+        // Hardcoded file name for testing
+        String fileName = "p1-in.txt";
+
+        try{
+            Scanner scanner = new Scanner(new File(fileName));
+
+        }
+        catch (FileNotFoundException pException){
+            System.out.printf("Oops, could not open '%s' for reading. The program is ending.", fileName);
+        }
+
     };
 }
