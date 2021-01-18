40a1ebfc48e3764b6225eec4f5c52d6380b063d8
TJ Salgado <tsalgado96@gmail.com>
2021-01-18 11:00:01 +0100

    Added methods findRuns(), arrayListCreate(), and readInputFile()

--- Main.java
+++ Main.java
@@ -15,15 +15,19 @@
  ********************************************************************************************************/
 import java.util.Scanner;
 import java.io.File;
-import java.io.PrintWriter;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.lang.Integer;
 
 public class Main {
-    public static void main(String[] pArgs) {
+    public static void main(String[] pArgs){
         new Main().run();
     }
 
     private void run(){
+        // ArrayList for contents of the file
+        ArrayList<Integer> list = new ArrayList<>();
+
         // User inputs the name of the file
 //        Scanner input = new Scanner((System.in));
 //        System.out.print("Please enter file name: ");
@@ -32,13 +36,65 @@ public class Main {
         // Hardcoded file name for testing
         String fileName = "p1-in.txt";
 
-        try{
-            Scanner scanner = new Scanner(new File(fileName));
-
+        // Try catch block for reading file
+        try {
+            list = readInputFile(fileName);
         }
         catch (FileNotFoundException pException){
             System.out.printf("Oops, could not open '%s' for reading. The program is ending.", fileName);
+            System.exit(-100);
         }
 
-    };
-}
+        ArrayList<Integer> listRunsUpCount = new ArrayList<>();
+        ArrayList<Integer> listRunsDnCount = new ArrayList<>();
+
+        // 1 signifies RUNS_UP
+        findRuns(list, 1);
+        // -1 signifies RUNS_DN
+        findRuns(list, -1);
+
+    } // end run()
+
+
+    public ArrayList<Integer> findRuns(ArrayList<Integer> pList, int pDir){
+        ArrayList<Integer> listRunsCount  = arrayListCreate(pList.size(), 0);
+        int i = 0, k = 0;
+
+        while(i < pList.size()){
+            if (pDir == 1 && pList.get(i) <= pList.get(i + 1)){
+                k++;
+            }
+            else if (pDir == -1 && pList.get(i) >= pList.get(i)){
+                k++;
+            }
+            else {
+                if (k != 0){
+
+                }
+            }
+
+            i++;
+        }
+    } // end findRuns()
+
+
+    public ArrayList<Integer> arrayListCreate(int pSize, int pInitValue){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0;  i < pSize; i++){
+            list.add(pInitValue);
+        }
+        return list;
+    } // end arrayListCreate()
+
+    // Read the file that is passed and add integers to the ArrayList list
+    public ArrayList<Integer> readInputFile(String pFileName) throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File(pFileName));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(scanner.hasNextInt()){
+            int num = scanner.nextInt();
+            list.add(num);
+        }
+        return list;
+    } // end readInputFile
+} // end Main
